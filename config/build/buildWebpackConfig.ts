cb2608c4e9ba203300e1353903f3394a34212e8b
nmr <nmrhard@gmail.com>
2023-02-05 23:22:22 +0200

    add dev server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,13 +1,14 @@
 import { BuildOptions } from './types/config';
 import webpack from 'webpack';
-import { buildPlugins } from './buildPlugin';
+import { buildPlugins } from './buildPlugins';
 import { buildLoader } from './buildLoader';
 import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
 export const buildWebpackConfig = (
   options: BuildOptions
 ): webpack.Configuration => {
-  const { mode, paths } = options;
+  const { mode, paths, isDev } = options;
 
   return {
     mode,
@@ -22,5 +23,7 @@ export const buildWebpackConfig = (
       rules: buildLoader(),
     },
     resolve: buildResolvers(),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   };
 };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { someFn } from './test';
 
 someFn(0);
+
+document.body.innerHTML = '<h1>Hello Wold!</h1>';
