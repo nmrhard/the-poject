3c6a3289e4e2644247623193f6a7d9ff1c152e45
nmr <nmrhard@gmail.com>
2023-02-09 00:34:39 +0200

    Add router and code splitting

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -5,5 +5,6 @@ export const buildDevServer = (
   options: BuildOptions
 ): DevServerConfiguration => ({
   port: options.port,
-  open: true
+  open: true,
+  historyApiFallback: true,
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import classes from './App.module.scss';
+import * as React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+const Home = React.lazy(() => import('./pages/Home'));
+const About = React.lazy(() => import('./pages/About'));
 import './index.scss';
 
 const App = () => (
   <div className='app'>
-    <h1 className={classes.heading}>Hello World!</h1>
+    <Link to={'/'}>Home</Link>
+    <Link to={'/about'}>About</Link>
+    <React.Suspense fallback={<p>Loading...</p>}>
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/about'} element={<About />} />
+      </Routes>
+    </React.Suspense>
   </div>
 );
 
