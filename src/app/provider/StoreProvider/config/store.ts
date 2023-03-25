0b2ad70a1996a991504d5b4b7649fb7ffb261df5
nmr <nmrhard@gmail.com>
2023-03-25 15:48:44 +0200

    add input, add login, form ui

--- src/app/provider/StoreProvider/config/store.ts
+++ src/app/provider/StoreProvider/config/store.ts
@@ -1,13 +1,18 @@
 /* eslint-disable implicit-arrow-linebreak */
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
+import { userReducer } from 'entities/User';
 import { StateSchema } from './StateSchema';
 
-export const createReduxStore = (initialState?: StateSchema) =>
-  configureStore<StateSchema>({
-    reducer: {
-      counter: counterReducer,
-    },
+export const createReduxStore = (initialState?: StateSchema) => {
+  const rootReducer: ReducersMapObject<StateSchema> = {
+    counter: counterReducer,
+    user: userReducer,
+  };
+
+  return configureStore<StateSchema>({
+    reducer: rootReducer,
     devTools: __IS_DEV__,
     preloadedState: initialState,
   });
+};
--- src/widget/Navbar/ui/Navbar.tsx
+++ src/widget/Navbar/ui/Navbar.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable i18next/no-literal-string */
 /* eslint-disable react/self-closing-comp */
+import { LoginModal } from 'features/AuthByUsername';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import { Modal } from 'shared/ui/Modal/Modal';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -15,24 +15,24 @@ const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
   const [isAuthModal, setAuthModal] = React.useState(false);
 
-  const onToggleModal = React.useCallback(() => {
-    setAuthModal((prev) => !prev);
+  const onCloseModal = React.useCallback(() => {
+    setAuthModal(false);
+  }, []);
+
+  const onShowModal = React.useCallback(() => {
+    setAuthModal(true);
   }, []);
 
   return (
     <div className={classNames(styles.navbar, {}, [className])}>
       <Button
         className={styles.links}
-        onClick={onToggleModal}
+        onClick={onShowModal}
         theme={ThemeButton.CLEAR_INVERTED}
       >
         {t('Войти')}
       </Button>
-      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-        commodi consequatur eligendi impedit incidunt necessitatibus possimus
-        quis saepe sunt totam.\n
-      </Modal>
+      <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
     </div>
   );
 };
